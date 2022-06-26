082245d50bcddc15ca7495d2233a667616365aca
nancycrojas <nanclarisaok@gmail.com>
2022-06-26 18:47:46 -0300

    Funciones - ejercicio 01

---
+++ app.js
@@ -0,0 +1,15 @@
+//sumar(a, b)
+//Crear una función sumar que tome como argumentos dos números y devuelva la suma de ellos
+
+//sumar(2, 3) // 5
+//sumar(1.2, 3.4) // 4.6
+//sumar(3, -5) // -2
+
+const sumar = (a, b) => {
+//function sumar (a, b) {
+    return a + b;
+}
+
+sumar (2, 3);
+sumar(1.2, 3.4);
+sumar(3, -5); 
